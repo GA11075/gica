30a31fa963cd17cb058c38bde3ed0567f6f85197
GI HORA <gialveshora@gmail.com>
2024-11-13 07:52:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Qual animal representa o símbolo da paz?',
+    'Pomba Branca.'
+)
+
+criaCartao(
+    'Geral',
+    'O sushi é uma prato original de qual país?',
+    'Japão.'
+)
+
+criaCartao(
+    'Inglês',
+    'Quantos lados tem um dado?',
+    'Seis lados.'
+)
+
+criaCartao(
+    'Geral',
+    'O vinho é feito com qual fruto?',
+    'Uva.'
+)
